8b04dddbde51dfa2c16c022df93e38d994c8eef0
subhani511 <shaiksubhanibasha982374@gmail.com>
2025-09-09 09:52:44 +0530

    new update

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-let API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
-
-if (!API_BASE.endsWith("/api")) {
-  API_BASE = API_BASE.replace(/\/$/, "") + "/api";
-}
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: API_BASE + "/api", // always add /api once
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
